b804ea3fe61c7ca2c8600f868f7e5effdde37d2e
JJ Ashcraft <jj@jjashcraft.com>
2018-09-04 19:31:57 -0400

    recipe_batches completed

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,22 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass
-
+  batches = 0
+  amounts = []
+  for rec_key, rec_val in recipe.items():
+    for ing_key, ing_val in ingredients.items():
+      if len(recipe) != len(ingredients):
+        return 0
+      elif rec_key == ing_key and ing_val >= rec_val:
+        print(f'recipe:{rec_key}:{rec_val}, amount on hand:{ing_key}:{ing_val}')
+        amount = int(ing_val/rec_val)
+        print(f'{amount}') #add an array to push these values to, and then sort by lowest, if lowest greater than 1 return
+        amounts.append(amount)
+       
+  amounts.sort()
+  print(f'amounts:{amounts}')
+  batches = amounts[0]    
+  return amounts[0]
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
