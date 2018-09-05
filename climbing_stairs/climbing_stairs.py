6f3f40a2950bdff810d77888201fe274f27b800b
JJ Ashcraft <jj@jjashcraft.com>
2018-09-04 23:05:24 -0400

    climbing_stairs in progress

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,8 +2,24 @@
 
 import sys
 
-def climbing_stairs(n):
-  pass
+
+ways = []
+
+def climbing_stairs(n): #n is the number of stairs
+  #how many ways can the child climb the stairs
+  #for each permutation, push to ways array
+  if n == 0:
+    ways.append(1)
+    return ways[0]
+  else:
+    while n is not 0:
+      for i in range(n):
+        ways.append((n, n - i))
+        # climbing_stairs(n - 1)
+      print(ways)    
+    return ways  
+  #n=4 c:1,1,1,1  c:1,2,1   c:1,1,2  c:2,1,1  c:4  c:3,1   c:1,3   c:2,2
+
 
 
 if __name__ == "__main__":
